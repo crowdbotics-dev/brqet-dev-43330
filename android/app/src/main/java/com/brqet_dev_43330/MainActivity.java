ed4dfb5628d301c8061e23ea383b6c220e6f1005
Crowdbotics <engineering@crowdbotics.com>
2022-11-08 06:03:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/brqet_dev_43330/MainActivity.java
@@ -0,0 +1,15 @@
+package com.brqet_dev_43330;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "brqet_dev_43330";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "brqet_dev_43330",
+  url: "https://brqet_dev_43330.botics.co",
+  api: "https://brqet_dev_43330.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
